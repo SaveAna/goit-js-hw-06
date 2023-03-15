0ae5df950506a6e10d07c087e6d2570ba86be35f
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-15 22:32:48 +0200

    task-02

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,7 @@ console.log(`Number of categories: ${list.children.length}`);
 
 const listItem = document.querySelectorAll('li.item');
 listItem.forEach(element => {
+  console.log('');
   console.log(`Category: ${element.firstElementChild.textContent}`);
   console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const list = document.querySelector('ul#ingredients');
+
+const listItems = ingredients.map(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('item');
+  listItem.textContent = ingredient;
+  return listItem;
+});
+
+list.append(...listItems);
+console.log(list);
