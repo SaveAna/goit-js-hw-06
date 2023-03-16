4dcd586edc6587a14c5090f77f2ef1e0545bbda3
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-16 04:38:27 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const colorValue = document.querySelector('.color');
+const colorBtn = document.querySelector('.change-color');
+
+colorBtn.addEventListener('click', onBtnClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
 }
