57dd28fe1ae63d414e09a3d18b5765fc3879043e
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-16 04:17:58 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+
+  if (formData.get('email') === '') {
+    alert('Enter your email!');
+    return;
+  }
+
+  if (formData.get('password') === '') {
+    alert('Enter your password!');
+    return;
+  }
+
+  formData.forEach((value, name) => {
+    console.log(`${name}: ${value}`);
+  });
+
+  form.reset();
+}
